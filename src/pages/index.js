2f3b10459656918f5fbf0ff13310d915619943c6
David Nascimento <davidpn.11@gmail.com>
2018-08-07 23:25:14 -0300

    adding style

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,44 @@
 import React from 'react'
 import Link from 'gatsby-link'
-
+import profile from '../images/profile.jpg'
+import '../layouts/app.scss'
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+  <div className="wrapper">
+    <header className="header">
+      <img className="profile" src={profile} />
+    </header>
+    <div className="content">
+      <span className="title">David Nascimento</span>
+      <span className="subtitle">Front End Developer</span>
+      <span className="text-body">
+        Currently working full time at
+        <a className="take-link" href="http://take.net/" target="blank">
+          Take
+        </a>
+      </span>
+      <div className="social">
+        <span className="social-icon facebook">
+          <a href="https://www.facebook.com/davidpn.11" target="blank">
+            <i className="fa fa-2x fa-facebook" />
+          </a>
+        </span>
+        <span className="social-icon github">
+          <a href="https://github.com/davidpn11" target="blank">
+            <i className="fa fa-2x fa-github" />
+          </a>
+        </span>
+        <span className="social-icon linkedin">
+          <a href="https://www.linkedin.com/in/davidpn11/" target="blank">
+            <i className="fa fa-2x fa-linkedin" />
+          </a>
+        </span>
+        <span className="social-icon twitter">
+          <a href="https://twitter.com/11Davidpn" target="blank">
+            <i className="fa fa-2x fa-twitter" />
+          </a>
+        </span>
+      </div>
+    </div>
   </div>
 )
 
